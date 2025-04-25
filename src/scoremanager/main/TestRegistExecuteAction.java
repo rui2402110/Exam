b72ceb2c3350eadade127328a392d844808c7fa3
djangouser <tky2402005@stu.o-hara.ac.jp>
2025-04-25 12:36:12 +0900

    ★データ取れてないヨ★TREA.java あべ

--- src/scoremanager/main/TestRegistExecuteAction.java
+++ src/scoremanager/main/TestRegistExecuteAction.java
@@ -1,5 +1,6 @@
 package scoremanager.main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,13 +12,13 @@ import bean.Student;
 import bean.Subject;
 import bean.Teacher;
 import bean.Test;
-import bean.TestListSubject;
 import dao.ClassNumDao;
 import dao.StudentDao;
 import dao.SubjectDao;
 import dao.TestDao;
+import tool.Action;
 
-public class TestRegistExecuteAction {
+public class TestRegistExecuteAction extends Action{
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
@@ -45,56 +46,113 @@ public class TestRegistExecuteAction {
 		String subjectCd="";
 		String subjectName="";
 		String no ="";
-		List<TestListSubject> list;
+		// List<Test> の作成
+		List<Test> testList = new ArrayList<>();
+
 
 		//JSPから送られたデータを取得
 		entYear  = req.getParameter("f1");//入学年度
 		classNum = req.getParameter("f2");//クラス
 		subjectCd = req.getParameter("f3");//科目名？科目コード？
 		no = req.getParameter("f4");//学生番号
+		String paramName = "point_" + no; // パラメータ名を構築
+		String point = req.getParameter(paramName); // 得点を取得
+
 
 		//入力値チェック
-		if
-		//入学年度、クラス、科目、回数のいずれかが未入力の場合
-		if (entYear!=null && classNum != null && subjectCd != null && no != null){
-			//subject
-			//科目コード
-			subject= subDao.get(subjectCd,teacher.getSchool());
-			subject.setName(subject.getName());
+		if(point==null){
 
-			//student
-			//入学年度 String ->int
-			student.setEntYear(Integer.parseInt(entYear));
-			//氏名
-			student.setName(student.getName());
+			//入学年度、クラス、科目、回数のいずれかが未入力の場合
+			if (entYear!=null && classNum != null && subjectCd != null && no != null){
+				//subject
+				//科目コード
+				subject= subDao.get(subjectCd,teacher.getSchool());
+				subject.setName(subject.getName());
 
-			//test
-			test =tDao.get(student, subject,teacher.getSchool(), Integer.parseInt(no));
+				//student
+				//入学年度 String ->int
+				student.setEntYear(Integer.parseInt(entYear));
+				//氏名
+				student.setName(student.getName());
+
+				//test
+				test =tDao.get(student, subject,teacher.getSchool(), Integer.parseInt(no));
 
 
-			//情報格納
-			//科目
-			test.setSubject(subject);
-			//受験回数
-			test.setNo(test.getNo());
-			//入学年度、氏名
-			test.setStudent(student);
-			//クラス
-			test.setClassNum(classNum);
-			//学生番号
-			test.setNo(Integer.parseInt(no));
-			req.getRequestDispatcher("test_regist.jsp").forward(req, res);
+				//情報格納
+				//科目
+				test.setSubject(subject);
+				//受験回数
+				test.setNo(test.getNo());
+				//入学年度、氏名
+				test.setStudent(student);
+				//クラス
+				test.setClassNum(classNum);
+				//学生番号
+				test.setNo(Integer.parseInt(no));
+				req.getRequestDispatcher("test_regist.jsp").forward(req, res);
 
+			}else{
+				// 認証失敗の場合
+				// エラーメッセージをセット
+				String errors = ("入学年度とクラスと科目と回数を選択してください");
+				req.setAttribute("errors", errors);
+
+				String url = "test_regist.jsp";
+				req.getRequestDispatcher(url).forward(req, res);
+			}
 		}else{
-			// 認証失敗の場合
-			// エラーメッセージをセット
-			String errors1 = ("入学年度とクラスと科目と回数を選択してください");
-			req.setAttribute("errors1", errors1);
+			if (Integer.parseInt(point) >= 0 && Integer.parseInt(point) <= 100){
+				//subject
+				//科目コード
+				subject= subDao.get(subjectCd,teacher.getSchool());
+				subject.setName(subject.getName());
 
-			String url = "test_regist.jsp";
-			req.getRequestDispatcher(url).forward(req, res);
+				//student
+				//入学年度 String ->int
+				student.setEntYear(Integer.parseInt(entYear));
+				//氏名
+				student.setName(student.getName());
+
+				//test
+				test =tDao.get(student, subject,teacher.getSchool(), Integer.parseInt(no));
+
+
+				//情報格納
+				//科目
+				test.setSubject(subject);
+				//受験回数
+				test.setNo(test.getNo());
+				//入学年度、氏名
+				test.setStudent(student);
+				//クラス
+				test.setClassNum(classNum);
+				//学生番号
+				test.setNo(Integer.parseInt(no));
+				//得点
+				test.setPoint(Integer.parseInt(point));
+
+				// テストデータをリストに追加
+				testList.add(test);
+
+				// DAOで保存処理（複数テストデータを一括で保存）
+				tDao.save(testList);
+
+				// 確認ログ
+				System.out.println("保存されたテストデータの件数: " + testList.size());
+
+
+				// フォワード
+				req.getRequestDispatcher("test_regist.jsp").forward(req, res);
+			}else{
+				// 認証失敗の場合
+				// エラーメッセージをセット
+				String errors = ("0~100の範囲で入力してください");
+				req.setAttribute("errors", errors);
+
+				String url = "test_regist.jsp";
+				req.getRequestDispatcher(url).forward(req, res);
+			}
 		}
-
-		if()
 	}
 }
