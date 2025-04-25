6cdac098990318ed7a6278260deafed3d99d89e7
rui <tky2402110@stu.o-hara.ac.jp>
2025-04-25 09:53:05 +0900

    TestList関連 :新橋

---
+++ src/scoremanager/main/TestListStudentExecuteAction.java
@@ -0,0 +1,20 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import tool.Action;
+
+public class TestListStudentExecuteAction extends Action {
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		//メソッドとスタブ
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		// フォワード
+	    req.getRequestDispatcher("test_list_student.jsp").forward(req, res);
+	}
+
+}
