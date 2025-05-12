597af91a3a62555068b41f7a7c43c43a9139c623
rui <tky2402110@stu.o-hara.ac.jp>
2025-05-12 09:41:49 +0900

    TestDao 修正 ;新橋

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -221,7 +221,7 @@ public class TestDao extends Dao {
                 statement.setInt(4, test.getNo());
                 statement.setInt(5, test.getPoint());
                 statement.setString(6, test.getClassNum());
-                statement.setString(1, test.getStudent().getNo());
+                statement.setString(7, test.getStudent().getNo());
             }
             // 実行して影響を受けた行数を確認
             int affected = statement.executeUpdate();
